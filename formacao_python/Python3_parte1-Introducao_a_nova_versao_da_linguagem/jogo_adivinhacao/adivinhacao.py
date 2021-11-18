7eaabbf714c8fe2b858cc438f1b85097a1bdfad0
Carlos Ferreira <89271917+cadu08@users.noreply.github.com>
2021-11-18 16:01:21 -0300

    chp7_adivinhacao.py - end of the game
    This is the last version of the 'guessing' game presented at this Alura course. On This class was added different levels for the game and was also implemented a points system based on the distance between the trial value and the random value. The function abs( ) was introduced to calculate the variable 'lost_points' that represents the lost points in the trial.

--- formacao_python/Python3_parte1-Introducao_a_nova_versao_da_linguagem/jogo_adivinhacao/adivinhacao.py
+++ formacao_python/Python3_parte1-Introducao_a_nova_versao_da_linguagem/jogo_adivinhacao/adivinhacao.py
@@ -5,8 +5,20 @@ print("Bem vindo no jogo de adivinhação!")
 print("*********************************")
 
 numero_secreto = random.randrange(1,101)  #Gera um número aleatório entre 1 e 100
-total_de_tentativas = 3
-rodada = 1
+total_de_tentativas = 0
+pontos = 1000
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if nivel == 1:
+    total_de_tentativas = 20
+elif nivel == 2:
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas+1):
 
@@ -26,11 +38,13 @@ for rodada in range(1, total_de_tentativas+1):
     menor = chute < numero_secreto
 
     if chute == numero_secreto:
-        print("você acertou")
+        print(f"você acertou e fez {pontos} pontos")
         break
     elif maior:
         print("você errou. O seu chute foi maior que o número secreto!")
     elif menor:
         print("Você errou. O seu chute foi menor que o número secreto!")
+    pontos_perdidos = abs(numero_secreto - chute)
+    pontos = pontos - pontos_perdidos
 
 print("Fim do jogo")
