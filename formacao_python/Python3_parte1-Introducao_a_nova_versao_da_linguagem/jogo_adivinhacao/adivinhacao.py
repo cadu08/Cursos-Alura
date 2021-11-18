fa860fd1588c8e6b65a042a9485a25734a6909ae
Carlos Ferreira <89271917+cadu08@users.noreply.github.com>
2021-11-18 14:25:08 -0300

    chp5_adivinhacao.py
    This code refers to the game at the end of chapter 5 of this course. 
    
    It was changed the 'while loop' for a 'for loop'. Taught how to use "for in range" and the difference between 'break' and 'continue' commands.

--- formacao_python/Python3_parte1-Introducao_a_nova_versao_da_linguagem/jogo_adivinhacao/adivinhacao.py
+++ formacao_python/Python3_parte1-Introducao_a_nova_versao_da_linguagem/jogo_adivinhacao/adivinhacao.py
@@ -6,12 +6,16 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while (rodada <= total_de_tentativas):
+for rodada in range(1, total_de_tentativas+1):
 
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
-    chute = input("Digite o seu numero: ")
+    chute = input("Digite um número entre 1 e 100: ")
     chute = int(chute)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     print("Você digitou ", chute)
 
@@ -21,12 +25,10 @@ while (rodada <= total_de_tentativas):
 
     if chute == numero_secreto:
         print("você acertou")
-        total_de_tentativas = 4
+        break
     elif maior:
         print("você errou. O seu chute foi maior que o número secreto!")
     elif menor:
         print("Você errou. O seu chute foi menor que o número secreto!")
-
-    rodada = rodada + 1
 
 print("Fim do jogo")
