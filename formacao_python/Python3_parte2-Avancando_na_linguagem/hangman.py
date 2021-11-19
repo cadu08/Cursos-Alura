067192467b416683bee8928d2bf3eef1d6f2ac3c
Carlos Ferreira <89271917+cadu08@users.noreply.github.com>
2021-11-19 11:44:44 -0300

    Initial commit - Advancing in Python
    In the last course of this formation, the file 'hangman.py' has already created with prints as contents. However, in this course the game hangman will be really developed. On this commit was added the loop function of the game.

---
+++ formacao_python/Python3_parte2-Avancando_na_linguagem/hangman.py
@@ -0,0 +1,17 @@
+def play():
+    print ('***************************')
+    print ('Welcome to the hangman game')
+    print ('***************************')
+
+    secret_word = 'banana'
+
+    hanged = False
+    got_right = False
+
+    while(not hanged and not got_right):
+        print('hangman')
+
+    print('Game over')
+
+if __name__ == '__main__':
+    play()
