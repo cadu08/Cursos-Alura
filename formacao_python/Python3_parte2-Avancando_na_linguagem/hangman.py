8c168fc321272c5d24599206e6e5d89c0d3fb4c1
Carlos Ferreira <89271917+cadu08@users.noreply.github.com>
2021-11-24 21:17:03 -0300

    Using a list structure to save the guessed letters
    I

--- formacao_python/Python3_parte2-Avancando_na_linguagem/hangman.py
+++ formacao_python/Python3_parte2-Avancando_na_linguagem/hangman.py
@@ -4,6 +4,7 @@ def play():
     print ('***************************')
 
     secret_word = 'banana'
+    guesses_letters = ["_", "_", "_", "_", "_", "_"]
 
     hanged = False
     got_right = False
@@ -16,9 +17,11 @@ def play():
         for letter in secret_word:
             if trial.upper() == letter.upper():
                 print("The letter {} was found at index {}.".format(letter, index))
+                guesses_letters[index] = letter.upper()
             index = index + 1
 
         print('hangman')
+        print(guesses_letters)
 
     print('Game over')
 
