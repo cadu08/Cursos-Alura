f35265e4a0c12f86a2727f5ec71589be59eec226
Carlos Ferreira <89271917+cadu08@users.noreply.github.com>
2021-12-05 08:48:56 -0300

    Update hangman.py
    Using a list comprehension to initialize the list 'guesses_letters'

--- formacao_python/Python3_parte2-Avancando_na_linguagem/hangman.py
+++ formacao_python/Python3_parte2-Avancando_na_linguagem/hangman.py
@@ -3,27 +3,37 @@ def play():
     print ('Welcome to the hangman game')
     print ('***************************')
 
-    secret_word = 'banana'
-    guesses_letters = ["_", "_", "_", "_", "_", "_"]
+    secret_word = 'banana'.upper()
+    guesses_letters = ["_" for letter in secret_word]
 
     hanged = False
     got_right = False
+    mistakes = 0
 
     while(not hanged and not got_right):
 
-        trial = input("Type a letter: ").strip() #remove all blank spaces
+        trial = input("Type a letter: ").strip().upper() #remove all blank spaces and change the letter to the upper case
 
-        index = 0
-        for letter in secret_word:
-            if trial.upper() == letter.upper():
-                print("The letter {} was found at index {}.".format(letter, index))
-                guesses_letters[index] = letter.upper()
-            index = index + 1
+        if trial in secret_word:
+            index = 0
+            for letter in secret_word:
+                if trial == letter:
+                    print("The letter {} was found at index {}.".format(letter, index))
+                    guesses_letters[index] = letter.upper()
+                index += 1
+        else:
+            mistakes += 1
+            print("The letter you tipped is not on the word.")
+            print("You have {} trials yet.".format(len(secret_word) - mistakes))
 
-        print('hangman')
+        hanged = mistakes == 6
+        got_right = "_" not in guesses_letters
         print(guesses_letters)
 
-    print('Game over')
+    if got_right:
+        print("You win!")
+    else:
+        print('Game over')
 
 if __name__ == '__main__':
     play()
