088b7594751d7346bc2e9d615b4d06dac3c32af8
Carlos Ferreira <89271917+cadu08@users.noreply.github.com>
2021-12-05 18:12:55 -0300

    Update hangman.py
    In this commit was putted a better message about gamer win or lose. Moreover,  was inserted prints to represents the gamer being hanged. It is noteworthy that this commit reffers to the end of course Python part 2.

--- formacao_python/Python3_parte2-Avancando_na_linguagem/hangman.py
+++ formacao_python/Python3_parte2-Avancando_na_linguagem/hangman.py
@@ -15,29 +15,109 @@ def play():
 
     while not hanged and not got_right:
 
-        trial = input(
-            "Type a letter: ").strip().upper()  # remove all blank spaces and change the letter to the upper case
+        trial = ask_guess_trial()
 
         if trial in secret_word:
-            index = 0
-            for letter in secret_word:
-                if trial == letter:
-                    print("The letter {} was found at index {}.".format(letter, index))
-                    guesses_letters[index] = letter.upper()
-                index += 1
+            guesses_letters = mark_right_attempt(secret_word, guesses_letters, trial)
         else:
             mistakes += 1
-            print("The letter you tipped is not on the word.")
-            print("You have {} trials yet.".format(len(secret_word) - mistakes))
+            drawing_gallow(mistakes)
 
-        hanged = mistakes == len(secret_word)
+        hanged = mistakes == 7
         got_right = "_" not in guesses_letters
         print(guesses_letters)
 
     if got_right:
-        print("You win!")
+        print_winner_message()
     else:
-        print('Game over')
+        print_looser_message(secret_word)
+
+
+def print_winner_message():
+    print("Congrats, you win!")
+    print("  ___________   ")
+    print(" '._==_==_=_.'  ")
+    print(" .-\\:      /-. ")
+    print("| (|:.     |) | ")
+    print(" '-|:.     |-'  ")
+    print("   \\::.    /   ")
+    print("    '::. .'     ")
+    print("      ) (       ")
+    print("    _.' '._     ")
+    print("   '-------'    ")
+
+
+def print_looser_message(secret_word):
+    print("You were hanged, what a pity!")
+    print("The secret word was {}".format(secret_word))
+    print("    _______________      ")
+    print("   /               \     ")
+    print("  /                 \    ")
+    print("//                   \/\ ")
+    print("\|   XXXX     XXXX   | / ")
+    print(" |   XXXX     XXXX   |/  ")
+    print(" |   XXX       XXX   |   ")
+    print(" |                   |   ")
+    print(" \__      XXX      __/   ")
+    print("   |\     XXX     /|     ")
+    print("   | |           | |     ")
+    print("   | I I I I I I I |     ")
+    print("   |  I I I I I I  |     ")
+    print("   \_             _/     ")
+    print("     \_         _/       ")
+    print("       \_______/         ")
+
+
+def drawing_gallow(mistakes):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if mistakes == 1:
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if mistakes == 2:
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if mistakes == 3:
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if mistakes == 4:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if mistakes == 5:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if mistakes == 6:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if mistakes == 7:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
 
 
 def print_welcome_message():
@@ -60,6 +140,21 @@ def get_secret_word():
 
 def initialize_guesses_letters(secret_word):
     return ["_" for letter in secret_word]
+
+
+def ask_guess_trial():
+    trial = input(
+        "Type a letter: ").strip().upper()  # remove all blank spaces and change the letter to the upper case
+    return trial
+
+
+def mark_right_attempt(secret_word, guesses_letters, trial):
+    index = 0
+    for letter in secret_word:
+        if trial == letter:
+            guesses_letters[index] = letter.upper()
+        index += 1
+    return guesses_letters
 
 
 if __name__ == '__main__':
