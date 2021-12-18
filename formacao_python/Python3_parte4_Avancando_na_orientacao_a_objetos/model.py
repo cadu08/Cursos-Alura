8fb923d36c206dc8dbf07a3a709c5df43fb8b83b
Carlos Ferreira <89271917+cadu08@users.noreply.github.com>
2021-12-18 19:12:19 -0300

    Update model.py - Remembering Python encapsulation
    Using @property and setter, Python's own getter and setter methods.

--- formacao_python/Python3_parte4_Avancando_na_orientacao_a_objetos/model.py
+++ formacao_python/Python3_parte4_Avancando_na_orientacao_a_objetos/model.py
@@ -1,28 +1,54 @@
 class Movie:
 
     def __init__(self, name, year, duration):
-        self.name = name.title()
+        self.__name = name.title()
         self.year = year
         self.duration = duration
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
 
     def add_like(self):
-        self.likes += 1
+        self.__likes += 1
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name.title()
 
 class Serie:
 
     def __init__(self, name, year, seasons):
-        self.name = name.title()
+        self.__name = name.title()
         self.year = year
         self.seasons = seasons
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
 
     def add_like(self):
-        self.likes += 1
+        self.__likes += 1
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name.title()
 
 avengers = Movie('avengers - infinity war', 2018, 160)
-print(f'Name: {avengers.name} - Year: {avengers.year} - Duration: {avengers.duration}')
+print(f'Name: {avengers.name} - Year: {avengers.year} - Duration: {avengers.duration} - Likes: {avengers.likes}')
 
 theWitcher = Serie('the witcher', 2019, 2)
-print(f'Name: {theWitcher.name} - Year: {theWitcher.year} - Seasons: {theWitcher.seasons}')
+
+theWitcher.add_like()
+
+print(f'Name: {theWitcher.name} - Year: {theWitcher.year} - Seasons: {theWitcher.seasons} - Likes: {theWitcher.likes}')
