0db3646c7f05711ea5f0f5f0ef116cf0e5f9906c
Carlos Ferreira <89271917+cadu08@users.noreply.github.com>
2021-12-18 14:56:20 -0300

    Update model.py
    Creating a method to add likes to series and movies.

--- formacao_python/Python3_parte4_Avancando_na_orientacao_a_objetos/model.py
+++ formacao_python/Python3_parte4_Avancando_na_orientacao_a_objetos/model.py
@@ -1,17 +1,25 @@
 class Movie:
 
     def __init__(self, name, year, duration):
-        self.name = name
+        self.name = name.title()
         self.year = year
         self.duration = duration
+        self.likes = 0
+
+    def add_like(self):
+        self.likes += 1
 
 
 class Serie:
 
     def __init__(self, name, year, seasons):
-        self.name = name
+        self.name = name.title()
         self.year = year
         self.seasons = seasons
+        self.likes = 0
+
+    def add_like(self):
+        self.likes += 1
 
 avengers = Movie('avengers - infinity war', 2018, 160)
 print(f'Name: {avengers.name} - Year: {avengers.year} - Duration: {avengers.duration}')
